f631472a9eab289a81b16a2484e796f3c266ce04
BadWolf-alfa <marco@badwolf.one>
2017-06-17 03:45:10 +0200

    Various fixes for compile

--- kernel/compile.sh
+++ kernel/compile.sh
@@ -3,7 +3,11 @@ make mrproper
 VARIANT=$(date +"%d%m%Y")
 export CROSS_COMPILE=../kernel/CrossCompiler/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export LOCALVERSION="-Revolution_Kernel-v.$VARIANT"
+if [[ -f "$(pwd)/arch/arm64/configs/revolution_defconfig" ]]; then
 make ARCH=arm64 O=../out revolution_defconfig
+else
+make ARCH=arm64 O=../out merge_hi3660_defconfig
+fi
 make ARCH=arm64 O=../out -j8
 cp ../out/arch/arm64/boot/Image.gz bootimg/kernel.gz
 cd bootimg
